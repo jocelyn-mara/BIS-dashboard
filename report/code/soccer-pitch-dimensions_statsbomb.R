2c656372f3919af7500d45889758a714b040c0a8
jocelyn-mara <53923118+jocelyn-mara@users.noreply.github.com>
2022-04-01 14:46:22 +1100

    Add files via upload

---
+++ report/code/soccer-pitch-dimensions_statsbomb.R
@@ -0,0 +1,15 @@
+# pitch dimensions for ggsoccer plot
+# based on our custom statsbomb dimensions
+
+pitch_statsbomb_custom <- list(
+  length = 120,
+  width = 80,
+  penalty_box_length = 18,
+  penalty_box_width = 44,
+  six_yard_box_length = 6,
+  six_yard_box_width = 20,
+  penalty_spot_distance = 12,
+  goal_width = 8,
+  origin_x = 0,
+  origin_y = 0
+)
